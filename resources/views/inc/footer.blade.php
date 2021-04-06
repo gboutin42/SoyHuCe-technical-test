ff040ee79dff769467bd5668976aee3f9a6a3322
Boutin Grégory <boutin_gregory@yahoo.com>
2021-04-06 23:15:41 +0200

    remove other unused resources

--- resources/views/inc/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="bg-blue-600 text-red-50 text-center p-2">
-	<p>&copy; Copyright &middot; Boutin Grégory &middot; {{ date('M Y') }}</p>
-</footer>
--- resources/views/inc/header.blade.php
+++
@@ -1,3 +0,0 @@
-<header class="bg-blue-600 text-red-50 text-center text-3xl p-6">
-	<h1>Bienvenue sur {{ config('app.name') }}</h1>
-</header>
