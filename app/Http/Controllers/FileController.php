bdae78ffb0530938bc214fce7ce798282025ae0e
Boutin Grégory <boutin_gregory@yahoo.com>
2021-04-07 19:10:55 +0200

    add try catch to the methods API

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Image\Image;
@@ -11,124 +12,170 @@ class FileController extends Controller
 	// For upload new file and store on the server
 	function upload(Request $request)
 	{
-		$result = $request->file('image')
-                    ->storePubliclyAs('public/images',
-                                $request->file('image')
-                                    ->getClientOriginalName());
-		return ['result' => $result];
+        // return ['result' => $request->name];
+		try {
+            $result = $request->file('image')
+                        ->storePubliclyAs('public/images',
+                            $request->file('image')->getClientOriginalName());
+            return ['result' => $result, 'path' => storage_path()];
+        }
+        catch (Exception $e)
+        {
+            return $e->getMessage();
+        }
 	}
 
     // Get all files Images on the server
 	function filesList()
 	{
-		$result = Storage::allFiles('public/images');
-		return ['result' => $result];
+        try {
+            $result = Storage::allFiles('public/images');
+            return ['result' => $result];
+        }
+        catch (Exception $e)
+        {
+            return $e->getMessage();
+        }
 	}
 
     // Rename file
     function updateName(Request $req)
     {
-        if ($req->oldName && $req->newName)
+        try
         {
-            $oldPath =  "public/images/" . $req->oldName;
-            $newPath =  "public/images/" . $req->newName;
-            $image = Storage::move($oldPath, $newPath);
+            if ($req->oldName && $req->newName)
+            {
+                $oldPath =  "public/images/" . $req->oldName;
+                $newPath =  "public/images/" . $req->newName;
+                $image = Storage::move($oldPath, $newPath);
 
-            /*
-             * The following method is not used because the save method with
-             * $newPath makes a copy of the image and renames it
-             * and this is not the expected behavior
-             * but we keep it until we know more
-             */
-            // $oldPath = storage_path() . "/app/public/images/" . $req->oldName;
-            // $newPath = storage_path() . "/app/public/images/" . $req->newName;
-            // Image::load($oldPath)->save($newPath);
+                /*
+                 * The following method is not used because the save method with
+                 * $newPath makes a copy of the image and renames it
+                 * and this is not the expected behavior
+                 * but we keep it until we know more
+                 */
+                // $oldPath = storage_path() . "/app/public/images/" . $req->oldName;
+                // $newPath = storage_path() . "/app/public/images/" . $req->newName;
+                // Image::load($oldPath)->save($newPath);
+            }
+            else
+            {
+                return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                                        des parametre fourni (oldName, newName)"];
+            }
         }
-        else
+        catch (Exception $e)
         {
-            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
-                                    des parametre fourni (oldName, newName)"];
+            return $e->getMessage();
         }
     }
 
     // Resize File
     function updateSize(Request $req)
     {
-        if ($req && $req->fileName && $req->width && $req->height)
-        {
-            $pathName = storage_path() . "/app/public/images/" . $req->fileName;
-            Image::load($pathName)
-                    ->width($req->width)
-                    ->height($req->height)
-                    ->save();
+        try{
+            if ($req && $req->fileName && $req->width && $req->height)
+            {
+                $pathName = storage_path() . "/app/public/images/" . $req->fileName;
+                Image::load($pathName)
+                        ->width($req->width)
+                        ->height($req->height)
+                        ->save();
+            }
+            else
+            {
+                return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                                des parametre fourni (fileName, width, height)"];
+            }
         }
-        else
+        catch (Exception $e)
         {
-            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
-                            des parametre fourni (fileName, width, height)"];
+            return $e->getMessage();
         }
     }
 
     // Crop file size
     function updateCrop(Request $req)
     {
-        if ($req && $req->fileName && $req->width && $req->height
-            && $req->startX && $req->startY)
+        try
         {
-            $pathName = storage_path() . "/app/public/images/" . $req->fileName;
-            Image::load($pathName)
-                    ->manualCrop($req->width, $req->height,
-                                    $req->startX, $req->startY)
-                    ->save();
+            if ($req && $req->fileName && $req->width && $req->height
+                && $req->startX && $req->startY)
+            {
+                $pathName = storage_path() . "/app/public/images/" . $req->fileName;
+                Image::load($pathName)
+                        ->manualCrop($req->width, $req->height,
+                                        $req->startX, $req->startY)
+                        ->save();
+            }
+            else
+            {
+                return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                    des parametre fourni (fileName, width, height, startX, startY)"];
+            }
         }
-        else
+        catch (Exception $e)
         {
-            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
-                des parametre fourni (fileName, width, height, startX, startY)"];
+            return $e->getMessage();
         }
     }
 
     // Add filter
     function updateFilter(Request $req)
     {
-        if ($req && $req->fileName && $req->filter)
+        try
         {
-            $path = storage_path() . "/app/public/images/" . $req->fileName;
-            $image = Image::load($path);
+            if ($req && $req->fileName && $req->filter)
+            {
+                $path = storage_path() . "/app/public/images/" . $req->fileName;
+                $image = Image::load($path);
 
-            // check match filter
-            if ($req->filter == "sepia")
-            {
-                $image->sepia();
+                // check match filter
+                if ($req->filter == "sepia")
+                {
+                    $image->sepia();
+                }
+                else if ($req->filter == "grayscale")
+                {
+                    $image->greyscale();
+                }
+                $image->save();
             }
-            else if ($req->filter == "grayscale")
+            else
             {
-                $image->greyscale();
+                return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                                        des parametre fourni (fileName, filter)"];
             }
-            $image->save();
         }
-        else
+        catch (Exception $e)
         {
-            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
-                                    des parametre fourni (fileName, filter)"];
+            return $e->getMessage();
         }
     }
 
     // Get file for download
     function downloadFile($fileName)
     {
-        if ($fileName && $fileName != '')
+        try
         {
-            $path = storage_path() . '/app/public/images/' . $fileName;
-            $headers = array(
-                'Content-Type' => 'image/*'
-              );
-            return response()->download($path, $fileName, $headers);
+            if ($fileName && $fileName != '')
+            {
+                $path = storage_path() . '/app/public/images/' . $fileName;
+                $headers = array(
+                    'Content-Type' => 'image/*'
+                  );
+                return response()->download($path, $fileName, $headers);
+            }
+            else
+            {
+                return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                                                des parametre fourni (fileName)"];
+            }
         }
-        else
+        catch (Exception $e)
         {
-            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
-                                            des parametre fourni (fileName)"];
+            return $e->getMessage();
         }
     }
 }
