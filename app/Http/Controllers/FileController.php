392ee3a919e3eab2b5de329cb8ee940b4ad91fed
Boutin Grégory <boutin_gregory@yahoo.com>
2021-04-07 14:42:18 +0200

    clean and little secure

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Image\Image;
 
@@ -25,67 +24,97 @@ class FileController extends Controller
 		return ['result' => $result];
 	}
 
-    function updateName(Request $request)
+    function updateName(Request $req)
     {
-        if ($request->oldName && $request->newName)
+        if ($req->oldName && $req->newName)
         {
-            // $oldPath =  "public/images/" . $request->oldName;
-            // $newPath =  "public/images/" . $request->newName;
+            // $oldPath =  "public/images/" . $req->oldName;
+            // $newPath =  "public/images/" . $req->newName;
             // $image = Storage::move($oldPath, $newPath);
 
-            $oldPath = storage_path() . "/app/public/images/" . $request->oldName;
-            $newPath = storage_path() . "/app/public/images/" . $request->newName;
-            $image = Image::load($oldPath)->save($newPath);
-
-            return ['result' => $image];
+            $oldPath = storage_path() . "/app/public/images/" . $req->oldName;
+            $newPath = storage_path() . "/app/public/images/" . $req->newName;
+            Image::load($oldPath)->save($newPath);
         }
         else
         {
-            return ['erreur' => 'Parameters oldName or newName missing'];
+            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                                    des parametre fourni (oldName, newName)"];
         }
     }
 
-    function updateSize(Request $request)
+    function updateSize(Request $req)
     {
-        $pathName = storage_path() . "/app/public/images/" . $request->name;
-        Image::load($pathName)
-                ->width($request->width)
-                ->height($request->height)
-                ->save();
-    }
-
-    function updateCrop(Request $request)
-    {
-        $pathName = storage_path() . "/app/public/images/" . $request->name;
-        Image::load($pathName)
-                ->manualCrop($request->width, $request->height,
-                                $request->startX, $request->startY)
-                ->save();
-    }
-
-    function updateFilter(Request $request)
-    {
-        $name = storage_path() . "/app/public/images/" . $request->name;
-        $image = Image::load($name);
-        if ($request->filter == "sepia")
+        if ($req && $req->fileName && $req->width && $req->height)
         {
-            $image->sepia();
+            $pathName = storage_path() . "/app/public/images/" . $req->fileName;
+            Image::load($pathName)
+                    ->width($req->width)
+                    ->height($req->height)
+                    ->save();
         }
-        else if ($request->filter == "grayscale")
+        else
         {
-            $image->greyscale();
+            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                            des parametre fourni (fileName, width, height)"];
         }
-        $image->save();
+    }
 
-        return ["result" => $image];
+    function updateCrop(Request $req)
+    {
+        if ($req && $req->fileName && $req->width && $req->height
+            && $req->startX && $req->startY)
+        {
+            $pathName = storage_path() . "/app/public/images/" . $req->fileName;
+            Image::load($pathName)
+                    ->manualCrop($req->width, $req->height,
+                                    $req->startX, $req->startY)
+                    ->save();
+        }
+        else
+        {
+            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                des parametre fourni (fileName, width, height, startX, startY)"];
+        }
+    }
+
+    function updateFilter(Request $req)
+    {
+        if ($req && $req->fileName && $req->filter)
+        {
+            $path = storage_path() . "/app/public/images/" . $req->fileName;
+            $image = Image::load($path);
+            if ($req->filter == "sepia")
+            {
+                $image->sepia();
+            }
+            else if ($req->filter == "grayscale")
+            {
+                $image->greyscale();
+            }
+            $image->save();
+        }
+        else
+        {
+            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                                    des parametre fourni (fileName, filter)"];
+        }
     }
 
     function downloadFile($fileName)
     {
-        $path = storage_path() . '/app/public/images/' . $fileName;
-        $headers = array(
-            'Content-Type' => 'image/*'
-          );
-        return response()->download($path, $fileName, $headers);
+        if ($fileName && $fileName != '')
+        {
+            $path = storage_path() . '/app/public/images/' . $fileName;
+            $headers = array(
+                'Content-Type' => 'image/*'
+              );
+            return response()->download($path, $fileName, $headers);
+        }
+        else
+        {
+            return ["erreur" => "Une erreur est parvenu, vérifier l'ensemble
+                                            des parametre fourni (fileName)"];
+        }
     }
 }
